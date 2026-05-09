e718b458663b7c44281dd6e709dc19c8ef16b9d8
Rocio114 <r.olgunbugueo1@uandresbello.edu>
2026-05-09 17:54:36 -0400

    oo

--- src/preprocess.py
+++ src/preprocess.py
@@ -105,15 +105,13 @@ def group_rare_classes(df, min_samples=80):
         lambda x: x if x not in rare_families else 'OTHER'
     )
 
-    # MAPA LEGIBLE
-    label_map = (
-        df[['GRD_family', 'GRD']]
-        .drop_duplicates()
-        .set_index('GRD_family')['GRD']
-        .to_dict()
-    )
+    # label map
+    label_map = {
+        str(k): str(k)
+        for k in df['GRD_grouped'].unique()
+    }
 
-    return df
+    return df, label_map
 
 
 def get_feature_columns(df):
